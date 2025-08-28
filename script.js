0f3a28ef5dda2237c646e332f8a9262f6b8b06a8
GabrielCamargocoder <gacamargooliveira@gmail.com>
2025-08-28 17:01:01 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function mostrarSecao(id,event){
+
+   var secao = document.getElementsByClassName("cardapio2")
+
+   for(i = 0; i < secao.length; i++){
+
+    secao[i].style.display = "none";
+   }
+
+   document.getElementById(id).style.display = "flex"
+
+   event.preventDefault()
+
+}
